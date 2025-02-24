60f951ba867172746e2125eeeeeca22ca4efe719
ErayC4 <eray04.coskun@gmail.com>
2025-02-24 15:07:10 +0100

    tasks have now a many to one relationship to user, only correctly assiciated timeblocks get shown

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,9 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user! # Stellt sicher, dass nur angemeldete Nutzer Zugriff haben
   before_action :set_task, only: %i[ show edit update destroy ]
-
   # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   # GET /tasks/1 or /tasks/1.json
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
 
   # GET /tasks/new
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   # GET /tasks/1/edit
@@ -21,7 +21,7 @@ class TasksController < ApplicationController
 
   # POST /tasks or /tasks.json
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     respond_to do |format|
       if @task.save
@@ -60,7 +60,8 @@ class TasksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
-      @task = Task.find(params.expect(:id))
+      @task = current_user.tasks.find_by(id: params[:id])
+      redirect_to tasks_path, alert: "Nicht autorisiert!" if @task.nil?
     end
 
     # Only allow a list of trusted parameters through.
