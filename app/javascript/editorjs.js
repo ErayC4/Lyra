31ad85c34e128951b92203ab77fbe0b4413e2a99
ErayC4 <eray04.coskun@gmail.com>
2025-02-28 19:34:22 +0100

    prefills editor field, with whatever is in notes.content

--- app/javascript/editorjs.js
+++ app/javascript/editorjs.js
@@ -3,8 +3,11 @@ let editor; // Macht die Editor-Instanz global verfügbar
 document.addEventListener("DOMContentLoaded", function () {
   const editorElement = document.querySelector(".editorjs");
   const divId = editorElement.id;
+
+  //damit das inputfeld sich mit dem gesavetem content füllt
+  const savedContent = JSON.parse(document.getElementById('editorjs').dataset.savedContent);
   
-  // Editor-Initialisierung
+  // Editor-Initialisierung mit vorhandenen Daten
   editor = new EditorJS({
     holder: divId,
     tools: {
@@ -13,8 +16,9 @@ document.addEventListener("DOMContentLoaded", function () {
       code: CodeTool,
       paragraph: Paragraph
     },
+    data: savedContent, // Übergabe des vorhandenen Inhalts
     autofocus: true
   });
-
+  
   setupAutoSave(editorElement, editor);
 });
