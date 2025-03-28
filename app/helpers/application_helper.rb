2296a3cf2f6906bcf9c22671e53f7adfe9191075
ErayC4 <eray04.coskun@gmail.com>
2025-03-28 19:52:58 +0100

    simple style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def format_date(timestamp)
+    if timestamp.today?
+      "Heute"
+    elsif timestamp.to_date == Date.yesterday
+      "Gestern"
+    elsif Time.now - timestamp < 1.hour
+      "Gerade eben"
+    else
+      timestamp.strftime("%d. %B")
+    end
+  end
 end
