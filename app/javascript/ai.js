c0c36b6cc35daa139f6a142f4d7c899f9f36d338
ErayC4 <eray04.coskun@gmail.com>
2025-05-28 18:49:09 +0200

    changed spacing and style of chat-list-item and overall dropdown-menu

--- app/javascript/ai.js
+++ app/javascript/ai.js
@@ -234,8 +234,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     <p class="chat-title">${previewText}</p>
                     <svg 
                     xmlns="http://www.w3.org/2000/svg" 
-                    width="24" 
-                    height="24" 
+                    width="20" 
+                    height="20" 
                     viewBox="0 0 24 24" 
                     fill="none" 
                     stroke="currentColor" 
